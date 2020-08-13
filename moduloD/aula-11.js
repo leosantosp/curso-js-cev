7b040103950cb5834f8315d9e59925941a5dd9ce
leo.santosp <leo.santosp@outlook.com>
2020-08-12 22:40:18 -0300

    Aula 11 - Situações e Exemplos de Condição

---
+++ moduloD/aula-11.js
@@ -0,0 +1,25 @@
+// CONDIÇÃO SIMPLES - EXEMPLO
+var vel = 78
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+
+if (vel > 60) { // Condição simples!
+	console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+
+console.log(`Dirija sempre usando cinto de segurança.`)
+
+
+
+
+
+
+// CONDIÇÃO COMPOSTA - EXEMPLO
+
+var pais = 'EUA'
+console.log(`Vivendo em ${pais}`);
+
+if (pais == 'Brasil') {
+	console.log('Você é Brasileiro');
+} else {
+	console.log('Você é Estrangeiro!');
+}
